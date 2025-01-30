8c8d4805489e2906624b7cebf1e7b0fa9bb5ccb1
jonnydeppe <162109640+lunul-dev@users.noreply.github.com>
2025-01-30 16:29:38 +0200

    a

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Lunul Foundation</span>,
+  project: {
+    link: 'https://github.com/lunulfoundation',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/lunulfoundation',
+  footer: {
+    text: 'Lunul Foundation',
+  },
+}
+
+export default config
